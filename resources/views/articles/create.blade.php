58dd494084c28dc14259face6a66fb9f03852aed
Fabian Olsson <fabtasticdesign@live.se>
2019-03-09 13:11:26 +0100

    validation fix på valen vid skapande av artikel, även "old funktionen" för att behålla gammalt värde om validering inte lyckas

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts/app')
-
+@include('includes/status')
+@include('includes/validation_errors')
 @section('content')
   <div class="container mt-3">
 
@@ -8,27 +9,18 @@
       @csrf
 
       <div class="form-group">
-        <input type="text" name="name" id="name"class="form-control" placeholder="Article Name"><br>
-        <input type="text" name="description" id="description"class="form-control" placeholder="Description"><br>
-        <input type="number" name="rent_price" id="rent_price"class="form-control" placeholder="Price"><br>
+        <input type="text" name="name" id="name"class="form-control" placeholder="Article Name" value={{ old('name') }}><br>
+        <input type="text" name="description" id="description"class="form-control" placeholder="Description" value={{ old('description') }}><br>
+        <select name="category_id" >
+            <option value=1 >Plast</option>
+            <option value=2 >Trä</option>
+            <option value=3 >Stål</option>
+            <option value=4 >Alluminium</option>
+        </select>
+        <input type="number" name="rent_price" id="rent_price"class="form-control" placeholder="Price" value={{ old('rent_price') }}><br>
+        <input type="text" name="image_url" id="image_url"class="form-control" placeholder="Image URL" value={{ old('image_url') }}><br>
 
-    <div class="container">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label>Upload Image</label>
-                <div class="input-group">
-                        <span class="input-group-btn">
-                            <span class="btn btn-default btn-file">
-                                <input type="file" id="image_url">
-                            </span>
-                        </span>
-                </div>
-                    <img id='image-url'/>
-            </div>
-        </div>
-    </div>
-
-        <input type="submit" value="Create New Project" class="btn btn-primary">
+        <input type="submit" value="Create article" class="btn btn-primary">
       </div>
     </form>
   </div>
