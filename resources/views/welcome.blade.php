bf25e6f1870f747bb91654c36096f1561ebe9b57
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2019-03-10 11:31:07 +0100

    la in placehholder till hur gear kommer se ut på startsidan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,56 @@
-
 @extends('layouts.app')
 
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="flex-center position-ref full-height">
-                    <div class="content">
-                        <div class="title m-b-md">
-                            <h1>RMG Startpage</h1>
-                        </div>
-                    </div>
+<div class="container">
+    <div class="row justify-content">
+        <div class="col-3 ">
+            <div class="card " style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
+                <div class="card-body text-center">
+                    <h5 class="card-title ">Card title</h5>
+                    <p class="card-text ">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary ">Go somewhere</a>
                 </div>
             </div>
         </div>
-@endsection
+    
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    
+        <div class="col-3">
+            <div class="card " style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary  ">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+   
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    @endsection
