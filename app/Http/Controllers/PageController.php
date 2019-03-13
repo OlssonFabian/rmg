1c9d38d4af9c150624c9bd7fe08d3aeaf18398c8
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-03-13 14:11:19 +0100

    JM
    Kopplat category

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Article;
+use App\Category;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function welcome(Article $article) {
+    public function welcome(Article $article, Category $category) {
 		$msg = "Please make a registration to use our services";
 
 		$articles = $article->all();
+		$categories = $category->all();
 
 		if (Auth::check()) {
 			$user = Auth::user();
@@ -19,6 +21,6 @@ class PageController extends Controller
 			$msg = "You are logged in as {$user->name}!";
 		}
 
-		return view('welcome', ['msg' => $msg, 'articles' => $articles]);
+		return view('welcome', ['msg' => $msg, 'articles' => $articles, 'categories' => $categories]);
 	}
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,11 @@
 
 @section('content')
     <div class="container text-center">
-    <h1>Welcome to Rent My Gear</h1>
+    <h1>Welcome to Rent My Gear(Chair)</h1>
     <br><br><br>
+    @if ($material == $category->id)
+        
+    @endif
         @foreach ($articles->chunk(3) as $articleChunk)
             <div class="row">
                 @foreach($articleChunk as $article)
@@ -14,9 +17,14 @@
                             <img src="{{ $article->image_url }}" alt="..." style="max-height: 150px" class="img-responsive">
                             <div class="caption">
                                 <h3>{{ $article->name }}</h3>
-                                <p>{{ $article->description }}</p>
+                                <p>{{ $article->description }}</p>  
                                 <div class="clearfix">
                                     <div class="price">${{ $article->rent_price }}</div>
+                                    @foreach ($categories as $category)
+                                        @if ($article->category_id == $category->id)
+                                            <p>Material: {{ $category->name }}</p>
+                                        @endif
+                                    @endforeach
                                     <a href="{{ url('/orders/create') }}" class="btn btn-success" role="button">Order</a>
                                 </div>
                                 <br><br><br>
