c0c1109e26c7bfa5fd1a423669d4f23a1fb1ac65
Per Kristian <kristiansvanberg@gmail.com>
2019-03-13 08:48:28 +0100

    PK
    Fixat lite med utseende på historiken

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -9,19 +9,22 @@
     <hr style="border: 1px solid red;">
     <br>
         <ul class="list-group">
-            <li class="list-group-item"><span style="color: red;">Phone number:</span> {{ $user->phone_nr }}</li>
-            <li class="list-group-item"><span style="color: red;">Address:</span> {{ $user->address }}</li>
-            <li class="list-group-item"><span style="color: red;">Town:</span> {{ $user->town }}</li>
-            <li class="list-group-item"><span style="color: red;">E-mail:</span> {{ $user->email }}</li>
+            @if($order->user_id != Auth::id()) 
+                <li class="list-group-item"><span style="color: red;">Phone number:</span> {{ $user->phone_nr }}</li>
+                <li class="list-group-item"><span style="color: red;">Address:</span> {{ $user->address }}</li>
+                <li class="list-group-item"><span style="color: red;">Town:</span> {{ $user->town }}</li>
+                <li class="list-group-item"><span style="color: red;">E-mail:</span> {{ $user->email }}</li>
+            @endif
             <li class="list-group-item"><span style="color: red;">Start of Hire:</span> {{ $order->date_start }}</li>
             <li class="list-group-item"><span style="color: red;">End of Hire:</span> {{ $order->date_end }}</li>
-        @foreach($articles as $article)
-            <li class="list-group-item"><span style="color: red;">Article Name:</span> {{ $article->name }}</li>
-            <li class="list-group-item"><span style="color: red;">Article Description:</span> {{ $article->description }}</li>
-        @endforeach
             <li class="list-group-item"><span style="color: red;">Category:</span> {{ $category[0]->name }}</li>
+            @foreach($articles as $article)
+                <li class="list-group-item"><span style="color: red;">Article Name:</span> {{ $article->name }}</li>
+                <li class="list-group-item"><span style="color: red;">Article Description:</span> {{ $article->description }}</li>
+                <li class="list-group-item mx-auto en-bild"><img src="{{ $article->image_url }}" alt="En bild jävel" height="250" width="250"></li>
+            @endforeach
         </ul>
-        @if($order->name != Auth::user()->name) 
+        @if($order->user_id != Auth::id()) 
             <a href="/orders/{{ $order->id }}/edit" class="btn btn-primary mt-3">Manage Order</a>
         @endif
         <br>
