f5f455f26a137aeeb91f1bf5b728d0816947516e
Fabian Olsson <fabtasticdesign@live.se>
2019-03-13 08:50:23 +0100

    imagepill

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,13 +3,11 @@
 @include('partials/validation_errors')
 
 @section('content')
-	<div class="container mt-3 col-3">
-       <p>{{$articles->name}}</p>
+	<div class="container mt-3 col-7">
+       <h2>{{$articles->name}}</h2>
        <p>{{$articles->description}}</p>
-        <div class="col-12">
-              <img src="{{$articles->image_url}}" height="70vw">
+        <div class="col-9">
+              <img src="{{$articles->image_url}}" width="100vw">
         </div>
-
 	</div>
-
 @endsection
