91d60b90f42cad9d267b950fa0cd0423f2aa23a9
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2019-03-14 18:43:51 +0100

    Test AA

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -9,15 +9,15 @@
       @csrf
       @method('PUT')
       <div class="form-group">
-        <input type="text" name="name" id="name"class="form-control" placeholder="Article Name" value={{ old('name') }}><br>
-        <input type="text" name="description" id="description"class="form-control" placeholder="Description" value={{ old('description') }}><br>
+        <input type="text" name="name" id="name"class="form-control" placeholder="Article Name" value={{ $article->name or old('name') }}><br>
+        <input type="text" name="description" id="description"class="form-control" placeholder="Description" value={{ $article->description or old('description') }}><br>
         <select name="category_id" class="form-control" >
             @foreach($categories as $category)
             <option value="{{$category->id}}"> {{$category->name}} </option>
             @endforeach
         </select>
-        <input type="number" name="rent_price" id="rent_price"class="form-control" placeholder="Price" value={{ old('rent_price') }}><br>
-        <input type="text" name="image_url" id="image_url"class="form-control" placeholder="Image URL" value={{ old('image_url') }}><br>
+        <input type="number" name="rent_price" id="rent_price"class="form-control" placeholder="Price" value={{ $article->rent_price or old('rent_price') }}><br>
+        <input type="text" name="image_url" id="image_url"class="form-control" placeholder="Image URL" value={{ $article->image_url or old('image_url') }}><br>
 
         <input type="submit" value="Save Changes" class="btn btn-primary">
       </div>
