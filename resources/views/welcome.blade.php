f134159e1ed4faf23a11eb753808cf613262cefe
Fabian Olsson <fabtasticdesign@live.se>
2019-03-15 13:52:56 +0100

    Nav fungerar på sidan för att sortera

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,34 @@
         <button type="button" class="btn-btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">All<span class="caret"></span></button>
         <ul class="dropdown-menu">
             @foreach ($categories as $category)
-                <li> {{$category->name}} </li>
+                <li> <a href="/categories/{{$category->id}}"> {{$category->name}} </a> </li>
             @endforeach
-
         </ul>
+    </div>
+        @foreach ($articles->chunk(3) as $articleChunk)
+            <div class="row">
+                @foreach($articleChunk as $article)
+                    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail text-center">
+                            <img src="{{ $article->image_url }}" alt="..." style="max-height: 150px" class="img-responsive">
+                            <div class="caption">
+                                <h3>{{ $article->name }}</h3>
+                                <p>{{ $article->description }}</p>
+                                <div class="clearfix">
+                                    <div class="price">${{ $article->rent_price }}</div>
+                                    @foreach ($categories as $category)
+                                        @if ($article->category_id == $category->id)
+                                            <p>Material: {{ $category->name }}</p>
+                                        @endif
+                                    @endforeach
+                                    <a href="{{ url('/orders/create') }}" class="btn btn-success" role="button">Order</a>
+                                </div>
+                                <br><br><br>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
     </div>
 @endsection
