c4c5dd26b96ac905bfb732b72c209392ffb4e7e5
Per Kristian <kristiansvanberg@gmail.com>
2019-03-20 17:15:23 +0100

    PK
    Fixat så att man kan acceptera eller neka en order direkt inne på en orders info-view

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -64,7 +64,35 @@
             @endforeach
         </ul>
         @if($order->user_id != Auth::id() && $order->confirmed == 0 ) 
-            <a href="/orders/{{ $order->id }}/edit" class="btn btn-primary mt-3">Manage Order</a>
+            <h1>Confirm or Deny order {{ $order->id }}</h1>
+            
+            @include('partials/validation_errors')
+
+            <form method="POST" action="/orders/{{ $order->id }}">
+                @csrf
+                @method('PUT')
+                
+                <div class="form-group">
+                    <input type="hidden" name="article_id" id="article_id" class="form-control" placeholder="article_id" required readonly value="{{ old('article_id') ? old('article_id') : $order->article_id }}">
+                </div>
+
+                <div class="form-group">
+                    <input type="hidden" name="date_start" id="date_start" class="form-control"   placeholder="date_start" required readonly value="{{ old('date_start') ? old('date_start') : $order->date_start }}">
+                </div>
+                
+                <div class="form-group">
+                    <input type="hidden" name="date_end" id="date_end" class="form-control" placeholder="date_end" required readonly value="{{ old('date_end') ? old('date_end') : $order->date_end }}">
+                </div>
+                
+                <input type="submit" value="Confirm Order" class="btn btn-success mt-3">
+            </form>
+
+            <form method="POST" action="/orders/{{ $order->id }}">
+                @csrf
+                @method('DELETE')
+
+                <input type="submit" value="Deny Order" class="btn btn-danger mt-1">
+            </form>
         @endif
         <br>
         <br>
