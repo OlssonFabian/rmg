462121942a4a04f8059653351c220a816e6d1361
Per Kristian <kristiansvanberg@gmail.com>
2019-03-20 17:35:34 +0100

    PK
    stavfel - ändrat

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -8,7 +8,7 @@
 
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4 text-center">My Incomming Orders</h1>
+                <h1 class="display-4 text-center">My Incoming Orders</h1>
             </div>
         </div>
 
